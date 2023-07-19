b2ada8449129365d36dc5bb944468a70205f9069
Leon <leon@exemplo.com>
2023-07-19 14:55:45 -0300

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,108 @@
+﻿module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Abaixo ficam os plugins e suas configurações.
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less' // Saída/Entrada
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'], // ** Qualquer pasta , * Qualquer arquivo || "Qualquer arquivo less em qualquer pasta dentro de 'styles'"
+                tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+        replace: {
+            dev: { // Ambiente de DESENVOLVIMENTO
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS', // Palavra chave
+                            replacement: './styles/main.css' // Trocar a palavra chave por
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'], // Arquivo a serem feitas as trocas
+                        dest: 'dev/' // Pasta onde os arquivo alterados serão salvos
+                    }
+                ]
+            },
+            dist: { // Ambiente de PRODUÇÃO(ONLINE)     
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html' // Destino/Origem
+                }
+            }
+        },
+        clean: ['prebuild'],
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js' // Destino/Origem
+                }
+            }
+        }
+    })
+
+    // Carregamento das Tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Registro das Tasks
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+}
